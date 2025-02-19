8503750c056fd62c96caa3445cbce0565ef19fd4
Pham Quoc Khang <phamquockhang1108@gmail.com>
2025-02-19 23:05:19 +0700

    feat(product): add setItem in ProductMapper to update item type for product

--- src/main/java/com/pqkhang/ct553_backend/domain/category/mapper/ProductMapper.java
+++ src/main/java/com/pqkhang/ct553_backend/domain/category/mapper/ProductMapper.java
@@ -1,7 +1,9 @@
 package com.pqkhang.ct553_backend.domain.category.mapper;
 
 import com.pqkhang.ct553_backend.domain.category.dto.ProductDTO;
+import com.pqkhang.ct553_backend.domain.category.entity.Item;
 import com.pqkhang.ct553_backend.domain.category.entity.Product;
+import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
@@ -13,6 +15,15 @@ public interface ProductMapper {
 
     @Mapping(target = "item.itemId", source = "itemId")
     Product toProduct(ProductDTO productDTO);
+
+    @BeforeMapping
+    default void setItem(ProductDTO productDTO, @MappingTarget Product product) {
+        if (productDTO.getItemId() != null){
+           if(product.getItem() == null || !product.getItem().getItemId().equals(productDTO.getItemId())) {
+               product.setItem(Item.builder().itemId(productDTO.getItemId()).build());
+           }
+        }
+    }
 
     void updateProductFromDTO(ProductDTO productDTO, @MappingTarget Product product);
 }
